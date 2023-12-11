3483e3641fa11739f947066e01093798e212df1c
MFL <irpg17102545@gmail.com>
2023-12-11 10:17:05 +0700

    comment line 3 on vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === 'production' ? '/my-profile/' : '/',
+    // publicPath: process.env.NODE_ENV === 'production' ? '/my-profile/' : '/',
     transpileDependencies: true
 })
